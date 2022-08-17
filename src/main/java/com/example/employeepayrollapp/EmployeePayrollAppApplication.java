d6a654b5e4be7f8ff6e993ea61f8ad0fc87881e9
Megha <meghajagadale99@gmail.com>
2022-08-17 19:28:28 +0530

    create employee Payroll Spring App

---
+++ src/main/java/com/example/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.employeepayrollapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollAppApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Payroll App");
+        SpringApplication.run(EmployeePayrollAppApplication.class, args);
+    }
+
+}
