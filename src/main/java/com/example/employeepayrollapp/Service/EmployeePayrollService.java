ab6a6af26890ea759729c53661de0d6100c22349
Megha <meghajagadale99@gmail.com>
2022-08-18 00:11:42 +0530

    Saving Employee Payroll Data to MySql Database

--- src/main/java/com/example/employeepayrollapp/Service/EmployeePayrollService.java
+++ src/main/java/com/example/employeepayrollapp/Service/EmployeePayrollService.java
@@ -3,14 +3,20 @@ package com.example.employeepayrollapp.Service;
 import com.example.employeepayrollapp.Exception.EmployeePayrollException;
 import com.example.employeepayrollapp.dto.EmployeePayrollDTO;
 import com.example.employeepayrollapp.model.EmployeePayrollData;
+import com.example.employeepayrollapp.repository.EmployeePayrollRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
 public class EmployeePayrollService implements IEmployeePayrollService {
 
+    @Autowired
+    private EmployeePayrollRepository employeePayrollRepository;
     private List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
 
     @Override
@@ -27,9 +33,10 @@ public class EmployeePayrollService implements IEmployeePayrollService {
     @Override
     public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
         EmployeePayrollData empData = null;
-        empData = new EmployeePayrollData(employeePayrollList.size() + 1, empPayrollDTO);
+        empData = new EmployeePayrollData(empPayrollDTO);
         employeePayrollList.add(empData);
-        return empData;
+        log.debug("Employee Data: " + empData.toString());
+        return employeePayrollRepository.save(empData);
     }
 
     @Override
