d73b5787355cb5a4846dbf89820fb66351541d8d
Megha <meghajagadale99@gmail.com>
2022-08-17 21:56:55 +0530

    Using lombok library for logging

--- src/main/java/com/example/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/example/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -1,14 +1,17 @@
 package com.example.employeepayrollapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class EmployeePayrollAppApplication {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Payroll App");
         SpringApplication.run(EmployeePayrollAppApplication.class, args);
+        log.info("Employee Payroll App started");
     }
 
 }
--- src/main/java/com/example/employeepayrollapp/dto/ResponseDTO.java
+++ src/main/java/com/example/employeepayrollapp/dto/ResponseDTO.java
@@ -1,17 +1,15 @@
 package com.example.employeepayrollapp.dto;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
+@Getter
+@Setter
+@AllArgsConstructor
 @NoArgsConstructor
 public class ResponseDTO {
 
     private String message;
     private Object data;
 
-    public ResponseDTO(String message, Object data) {
-        this.message = message;
-        this.data = data;
-    }
 }
