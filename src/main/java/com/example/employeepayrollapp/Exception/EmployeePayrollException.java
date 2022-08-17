115a189f4abfa561d9d24e1b034610b2edcac094
Megha <meghajagadale99@gmail.com>
2022-08-17 22:28:08 +0530

    Provided user friendly error response in case of validation fails

---
+++ src/main/java/com/example/employeepayrollapp/Exception/EmployeePayrollException.java
@@ -0,0 +1,7 @@
+package com.example.employeepayrollapp.Exception;
+
+public class EmployeePayrollException extends RuntimeException {
+    public EmployeePayrollException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/example/employeepayrollapp/dto/ResponseDTO.java
+++ src/main/java/com/example/employeepayrollapp/dto/ResponseDTO.java
@@ -7,4 +7,9 @@ public @Data class ResponseDTO {
 
     private String message;
     private Object data;
+
+    public ResponseDTO(String message, Object data) {
+        this.message = message;
+        this.data = data;
+    }
 }
